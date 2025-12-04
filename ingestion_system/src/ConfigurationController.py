0c1a4b1428f4c306c29c78639f2d5f906e935618
carlomazzanti <carlomazzanti00@gmail.com>
2025-12-04 22:18:18 +0100

    finalized ConfigurationController.py

--- ingestion_system/src/ConfigurationController.py
+++ ingestion_system/src/ConfigurationController.py
@@ -13,5 +13,17 @@ class ConfigurationController:
     def get_ingestion_system_address(self):
         return self.current_config["ingestion_system"]
 
+    def get_preparation_system_address(self):
+        return self.current_config["preparation_system"]
+
+    def get_evaluation_system_address(self):
+        return self.current_config["evaluation_system"]
+
     def get_current_phase(self):
         return self.current_config["phase"]
+
+    def get_minimum_records(self):
+        return self.current_config["minimum_records"]
+
+    def get_missing_samples_threshold(self):
+        return self.current_config["missing_samples_threshold"]
